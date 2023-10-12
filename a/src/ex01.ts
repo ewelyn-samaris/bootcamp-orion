288f31841449bed57c23e422f64fbc55a7c81f1a
ewelyn.samaris <ewelynsamaris@hotmail.com>
2023-10-12 00:56:27 -0300

    final commit. Corrections made

--- a/src/ex01.ts
+++ a/src/ex01.ts
@@ -1,8 +1,8 @@
 function countVowels(st: string): number{
     // Função countVowels utiliza de regex por meio do método match para verificar a 1ª palavra recebida e retornar 
     // a quantidade de vogais presentes
-    let phrase = st.split(" ")[0]
-    const vowels = phrase.match(/[aeiouáéíóúâêîôûà]/ig)
+    let phrase: string = st.split(" ")[0]
+    const vowels: RegExpMatchArray | null = phrase.match(/[aeiouáéíóúâêîôûà]/ig)
     if(vowels){
         return vowels.length
     }else{
